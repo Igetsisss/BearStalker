44bc95e0d27b8a1614a22f32f272cf23811643ca
Igetsisss <98964802+Igetsisss@users.noreply.github.com>
2023-02-14 11:28:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,11 @@ function initialize() {
 
   // Create the request object for the DirectionsService
   var request = {
-    origin: new google.maps.LatLng(45.5011493, -122.8064663),
-    destination: new google.maps.LatLng(36.083370, -115.172997),
+    origin: new google.maps.LatLng(45.5011493, -122.8064663), // 1
+    
+    destination: new google.maps.LatLng(36.056595, -112.125198),
+    waypoints: [{location: new google.maps.LatLng(34.052235, -118.243683), stopover: false},
+      {location: new google.maps.LatLng(36.083370, -115.1729976), stopover: false}],
     travelMode: google.maps.TravelMode.DRIVING
   };
 
@@ -31,29 +34,62 @@ function initialize() {
       polyline.setMap(map);
     }
   });
+  //https://iili.io/H1mtvtf.png
  var image =
-    "https://iili.io/H1mtvtf.png";
+    "https://iili.io/HEDM13l.png";
   var beachMarker = new google.maps.Marker({
     position: { lat: 36.083370, lng: -115.1729976 },
     map,
     icon: image,
   
   });
+  var Marker = new google.maps.Marker({
+    position: { lat: 34.052235, lng: -118.243683 },
+    map,
+    icon: image,
+  
+  });
+  var image2 =
+    "https://iili.io/H1mtvtf.png";
+  var currentMarker = new google.maps.Marker({
+    position: { lat: 36.056595, lng: -112.125198 },
+    map,
+    icon: image2,
+  
+  });
+  
+  
   map.addListener("center_changed", () => {
     // 3 seconds after the center of the map has changed, pan back to the
     // marker.
     window.setTimeout(() => {
       map.panTo(marker.getPosition());
-    }, 3000);
+    }, 5000);
   });
    function openNav() {
     document.getElementById("mySidenav").style.width = "500px";
     document.getElementById("main").style.marginRight = "500px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
+function open2Nav() {
+  document.getElementById("mySidenav2").style.width = "500px";
+  document.getElementById("main").style.marginRight = "500px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 //openNav()
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function close2Nav() {
+    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.body.style.backgroundColor = "white";
+}
+function open3Nav() {
+  document.getElementById("mySidenav3").style.width = "500px";
+  document.getElementById("main").style.marginRight = "500px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+//openNav()
+function close3Nav() {
+    document.getElementById("mySidenav3").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
     document.body.style.backgroundColor = "white";
 }
@@ -62,5 +98,22 @@ function closeNav() {
     map.setCenter(beachMarker.getPosition());
     openNav()
     });
+    
+
+Marker.addListener("click", () => {
+  map.setZoom(7);
+  map.setCenter(Marker.getPosition());
+  open2Nav()
+  });
+  
+  currentMarker.addListener("click", () => {
+    map.setZoom(7);
+    map.setCenter(currentMarker.getPosition());
+    open3Nav()
+    });
+    
+
+
 }
+
 google.maps.event.addDomListener(window, "load", initialize);
